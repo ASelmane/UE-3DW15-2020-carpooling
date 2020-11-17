f9bcf9278eadff73bdea2aa9b19fc37a9d975282
ASelmane <47992754+ASelmane@users.noreply.github.com>
2020-11-17 23:48:30 +0100

    main: update table.sql

--- tables.sql
+++ tables.sql
@@ -11,3 +11,76 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
 (3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annonces`
+--
+
+DROP TABLE IF EXISTS `annonces`;
+CREATE TABLE IF NOT EXISTS `annonces` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lieuDepart` varchar(255) NOT NULL,
+  `lieuArrivee` varchar(255) NOT NULL,
+  `dateDepart` datetime NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `prix` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `annonces`
+--
+
+INSERT INTO `annonces` (`id`, `lieuDepart`, `lieuArrivee`, `dateDepart`, `place`, `prix`) VALUES
+(2, 'Rouen', 'Paris', '2020-11-17 16:20:00', '2', '20'),
+(3, 'Limoges', 'Paris', '2020-11-16 10:00:00', '1', '25'),
+(4, 'Rouen', 'Limoges', '2020-11-25 09:00:00', '1', '40');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE IF NOT EXISTS `cars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `marque` varchar(255) NOT NULL,
+  `modele` varchar(255) NOT NULL,
+  `couleur` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `cars`
+--
+
+INSERT INTO `cars` (`id`, `marque`, `modele`, `couleur`) VALUES
+(1, 'Toyota', 'yaris', 'noir'),
+(2, 'Peugeot', '308', 'grise'),
+(3, 'Citroen', 'C3', 'rouge');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idUser` int NOT NULL,
+  `idAnnonce` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `idUser`, `idAnnonce`) VALUES
+(1, 1, 2),
+(2, 2, 1),
+(3, 3, 3);
